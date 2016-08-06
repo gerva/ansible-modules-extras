9749a25831368dcf17e7a8f45e8d8fc235521b99
Massimo Gervasini <gervaland@hotmail.com>
2016-08-06 18:31:18 +0800

    Undocumented options in network/a10_server.py #2681

--- network/a10/a10_server.py
+++ network/a10/a10_server.py
@@ -78,6 +78,23 @@ options:
     required: false
     default: present
     choices: ['present', 'absent']
+  write_config:
+    description:
+      - If C(yes), any changes will cause a write of the running configuration
+        to non-volatile memory. This will save I(all) configuration changes,
+        including those that may have been made manually or through other modules,
+        so care should be taken when specifying C(yes).
+    required: false
+    default: "no"
+    choices: ["yes", "no"]
+  validate_certs:
+    description:
+      - If C(no), SSL certificates will not be validated. This should only be used
+        on personally controlled devices using self-signed certificates.
+    required: false
+    default: 'yes'
+    choices: ['yes', 'no']
+
 '''
 
 EXAMPLES = '''
